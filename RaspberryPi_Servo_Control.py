62a43138ec6f54281d14b4d399624b654daccaae
Esra Varlık <40757395+esravarlik@users.noreply.github.com>
2021-05-09 16:21:24 +0300

    Update RaspberryPi_Servo_Control.py

--- RaspberryPi_Servo_Control.py
+++ RaspberryPi_Servo_Control.py
@@ -5,6 +5,15 @@ servoPIN = 17
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(servoPIN, GPIO.OUT)
 
+#Duty Cycle  
+#(0,5/ 20) *100 = % 2.5 duty (0 degree)
+#(1,5/ 20) *100 = % 7.5 duty (90 degree)
+#(2,5/ 20) *100 = % 12.5 duty (180 degree)
+#(1/ 20) *100 = % 5 duty (45 degree)
+#(2/20)* 100 = %10 duty (135 degree)
+#Pwm equivalent according to angle:
+#Duty = (Angle/18) + 2,5
+
 pw = GPIO.PWM(servoPIN, 50)
 pw.start(2.5)
 try:
